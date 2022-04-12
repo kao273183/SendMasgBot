bc1e6176626c4aa3caa98e162a879d70af8fdef8
jack_kao <jack@bcowtech.com>
2022-04-12 23:17:59 +0800

    新增JSON寫入方式

--- SENDBOT.py
+++ SENDBOT.py
@@ -22,23 +22,30 @@ import time
 from telepot.loop import MessageLoop
 from pprint import pprint
 import os
+import json
 path_data = os.path.join(os.path.expanduser("~"), 'Desktop')
 bot = telepot.Bot('5121800363:AAFW06cDWT4-Ae9U_L__geQ1wLEPESlILxo')
-
-def handle(msg):
-    pprint(msg)
-    id = msg['chat']['id']
-    print(id)
-    if id != NULL:
-        keyfile = open(path_data + '\chatid'+".json","w",encoding="UTF-8")
-        keyfile.seek()
-        keyfile.write(str(id))
-        keyfile.truncate()
+if os.path.isfile(path_data + "\chatid.json"):
+        keyfile = open(path_data + '\chatid'+".json",encoding="UTF-8")
+        data = json.load(keyfile)
+        data['789'] = "titile"
+        print(data)
         keyfile.close()
-    #bot.sendMessage(msg['from']['id'],'test')
+        keyfile = open(path_data + '\chatid'+".json","w",encoding="UTF-8")
+        keyfile.write(json.dumps(data,sort_keys=True,indent=4,separators=(',', ': ')))
+        keyfile.close()
+"""def handle(msg):
+    pprint(msg)
+    #data = {'id':msg['chat']['id'],'title':msg['chat']['title']}
+    
+    if id != NULL:
+        keyfile = open(path_data + '\chatid'+".json","a",encoding="UTF-8")
+        keyfile.write(json.dumps(data))
+        keyfile.close()
+    #bot.sendMessage(msg['from']['id'],'test')"""
 
-MessageLoop(bot, handle).run_as_thread()
-print("I'm listening...")
+#MessageLoop(bot, handle).run_as_thread()
+#print("I'm listening...")
 
-while 1:
-    time.sleep(1)
+#while 1:
+#    time.sleep(1)
