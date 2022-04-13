3112a3e0db9291c357d10d270b6d77376cb0de93
jack_kao <jack@bcowtech.com>
2022-04-13 09:12:13 +0800

    修改語法，修短

--- SENDBOT.py
+++ SENDBOT.py
@@ -15,25 +15,62 @@ while 1:
     time.sleep(5)
 
 """
-from asyncio.windows_events import NULL
-from urllib.parse import _NetlocResultMixinBase
+#from asyncio.windows_events import NULL
+#from urllib.parse import _NetlocResultMixinBase
 import telepot
 import time
 from telepot.loop import MessageLoop
 from pprint import pprint
 import os
 import json
+
 path_data = os.path.join(os.path.expanduser("~"), 'Desktop')
 bot = telepot.Bot('5121800363:AAFW06cDWT4-Ae9U_L__geQ1wLEPESlILxo')
-if os.path.isfile(path_data + "\chatid.json"):
-        keyfile = open(path_data + '\chatid'+".json",encoding="UTF-8")
+dataPath = os.path.join(path_data, "chatid.json")
+
+def saveChatId(id=None,title=None):
+    if os.path.isfile(dataPath):
+        keyfile = open(dataPath,encoding="UTF-8")
         data = json.load(keyfile)
-        data['789'] = "titile"
-        print(data)
         keyfile.close()
-        keyfile = open(path_data + '\chatid'+".json","w",encoding="UTF-8")
-        keyfile.write(json.dumps(data,sort_keys=True,indent=4,separators=(',', ': ')))
-        keyfile.close()
+    else:
+        data = {}
+    if id != None:
+        data.update({id:title})
+    keyfile = open(dataPath,"w+",encoding="UTF-8")
+    keyfile.write(json.dumps(data))
+    keyfile.close()
+    return data
+
+def handle(msg):
+    global chatIds
+    pprint(msg)
+    id = msg['chat']['id']
+    title = msg['chat']['title']
+    if id not in chatIds:
+        chatIds = saveChatId(id,title)
+
+
+saveChatId("1qaz","2wsx")
+saveChatId("2wsx","2wsx")
+chatIds = saveChatId()
+print(chatIds)
+print("2wsx" in chatIds)
+print("3edc" in chatIds)
+
+"""if os.path.isfile(dataPath):
+    keyfile = open(dataPath,encoding="UTF-8")
+    data = json.load(keyfile)
+    data['789'] = "titile"
+    print(data)
+    keyfile.close()
+    keyfile = open(dataPath,"w",encoding="UTF-8")
+    keyfile.write(json.dumps(data,sort_keys=True,indent=4,separators=(',', ': ')))
+    keyfile.close()
+else:
+    keyfile = open(dataPath,"w",encoding="UTF-8")
+    keyfile.write(json.dumps({}))
+    keyfile.close()"""
 """def handle(msg):
     pprint(msg)
     #data = {'id':msg['chat']['id'],'title':msg['chat']['title']}
@@ -44,8 +81,8 @@ if os.path.isfile(path_data + "\chatid.json"):
         keyfile.close()
     #bot.sendMessage(msg['from']['id'],'test')"""
 
-#MessageLoop(bot, handle).run_as_thread()
-#print("I'm listening...")
+MessageLoop(bot, handle).run_as_thread()
+print("I'm listening...")
 
-#while 1:
-#    time.sleep(1)
+while 1:
+   time.sleep(1)
