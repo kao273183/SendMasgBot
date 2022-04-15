47084837229c33fc99ca851510c34fb6d5615e01
QMo <qmo@bcowtech.com>
2022-04-16 01:35:15 +0800

    release

--- SENDBOT.py
+++ SENDBOT.py
@@ -1,97 +1,97 @@
-"""import telegram
-import time
+import baseData as BD
+import json
+import os
 from pprint import pprint
-bot_token = '5121800363:AAFW06cDWT4-Ae9U_L__geQ1wLEPESlILxo'
-bot = telegram.Bot(token=bot_token)
-chat_id = '-627525390'
-
-def handle(msg):
-    pprint(msg)
-
-MessageLoop(bot, handle).run_as_thread()
-print("I'm listening...")
-
-while 1:
-    time.sleep(5)
-
-"""
-#from asyncio.windows_events import NULL
-#from urllib.parse import _NetlocResultMixinBase
-import telepot
 import time
 from telepot.loop import MessageLoop
-from pprint import pprint
-import os
-import json
+import telegram
 
-path_data = os.path.join(os.path.expanduser("~"), 'Desktop')
-bot = telepot.Bot('5121800363:AAFW06cDWT4-Ae9U_L__geQ1wLEPESlILxo')
-dataPath = os.path.join(path_data, "chatid.json")
+mediaGroup = []
 
-TesSend = "-627525390"
-TestBotGroup = "-771771446"
-
-def saveChatId(id=None,title=None):
+"""save Caht ID"""
+def saveChatId(dataPath,chatId=None,chatTitle=None):
     if os.path.isfile(dataPath):
-        keyfile = open(dataPath,encoding="UTF-8")
+        keyfile = open(dataPath,encoding=BD.encoding)
         data = json.load(keyfile)
         keyfile.close()
     else:
         data = {}
-    if id != None:
-        data.update({id:title})
-    keyfile = open(dataPath,"w+",encoding="UTF-8")
+    if chatId != None:
+        data.update({chatId:chatTitle})
+    keyfile = open(dataPath,"w+",encoding=BD.encoding)
     keyfile.write(json.dumps(data))
     keyfile.close()
     return data
 
+"""組group暫存"""
+def getGroup(mediaGroupId):
+    for jsonObj in mediaGroup:
+        if str(mediaGroupId) in jsonObj:
+            return jsonObj
+    mediaGroup.append({mediaGroupId:mediaGroupId,'media':[],'timestamp':time.mktime(time.localtime())})
+    for jsonObj in mediaGroup:
+        if str(mediaGroupId) in jsonObj:
+            return jsonObj
+
+"""組media"""
+def genMedia(msg):
+    data = {}
+    if 'audio' in msg:
+        data.update({'media':msg['audio']['file_id'],'type':'audio'})
+    if 'document' in msg:
+        data.update({'media':msg['document']['file_id'],'type':'document'})
+    if 'photo' in msg:
+        data.update({'media':msg['photo'][-1]['file_id'],'type':'photo'})
+    if 'video' in msg:
+        data.update({'media':msg['video']['file_id'],'type':'video'})
+    if 'caption' in msg:
+        data.update({'caption':msg['caption']})
+    return data
+
+"""find key"""
+def _find_first_key(d, keys):
+    for k in keys:
+        if k in d:
+            return k
+    raise KeyError('No suggested keys %s in %s' % (str(keys), str(d)))
+
 def handle(msg):
     global chatIds
-    pprint(msg)
-    id = msg['chat']['id']
-    title = msg['chat']['title']
-    message_id = msg['message_id']
-    pprint(id)
-    pprint(title)
-    pprint(message_id)
-    if id not in chatIds:
-        chatIds = saveChatId(id,title)
-    bot.forwardMessage(TestBotGroup, TesSend, message_id)
-    bot.sendMediaGroup(chat_id, media)
 
+    chatId = msg['chat']['id']
+    chatTitle = msg['chat']['title']
+    messageId = msg['message_id']
 
-# saveChatId("1qaz","2wsx")
-# saveChatId("2wsx","2wsx")
-chatIds = saveChatId()
-# print(chatIds)
-# print("2wsx" in chatIds)
-# print("3edc" in chatIds)
+    """檢查並儲存chat id"""
+    if str(chatId) not in chatIds:
+        chatIds = saveChatId(BD.from_path,chatId,chatTitle)
+    """轉傳"""
+    # bot.forwardMessage(TestBotGroup, TesSend, message_id)
+    """Media Group"""
+    # bot.sendMediaGroup(TestBotGroup, media=[{'media':"AgACAgUAAxkBAAOWYlZ-rQuHFx8KYAQ0QHSJWYZyWTcAAgKsMRuNf9BXjlwyHcayCC4BAAMCAANzAAMjBA",'type':'photo'}])
 
-"""if os.path.isfile(dataPath):
-    keyfile = open(dataPath,encoding="UTF-8")
-    data = json.load(keyfile)
-    data['789'] = "titile"
-    print(data)
-    keyfile.close()
-    keyfile = open(dataPath,"w",encoding="UTF-8")
-    keyfile.write(json.dumps(data,sort_keys=True,indent=4,separators=(',', ': ')))
-    keyfile.close()
-else:
-    keyfile = open(dataPath,"w",encoding="UTF-8")
-    keyfile.write(json.dumps({}))
-    keyfile.close()"""
-"""def handle(msg):
-    pprint(msg)
-    #data = {'id':msg['chat']['id'],'title':msg['chat']['title']}
+    if 'media_group_id' in msg:
+        mediaGroupId = msg['media_group_id']
+        gp = getGroup(mediaGroupId)
+        gpArr = gp['media']
+        gpArr.append(genMedia(msg))
+    else:
+        key = _find_first_key(msg,['text','audio','document','photo','video'])
+        if key == 'text':
+            BD.bot.forwardMessage(BD.targetChatId, chatId, messageId)
+        else:
+            BD.bot.sendMediaGroup(BD.targetChatId, media=[genMedia(msg)])
     
-    if id != NULL:
-        keyfile = open(path_data + '\chatid'+".json","a",encoding="UTF-8")
-        keyfile.write(json.dumps(data))
-        keyfile.close()
-    #bot.sendMessage(msg['from']['id'],'test')"""
+"""取得留存chat id紀錄"""
+chatIds = saveChatId(BD.from_path)
 
-MessageLoop(bot, handle).run_as_thread()
+MessageLoop(BD.bot, handle).run_as_thread()
 print("I'm listening...")
 
 while 1:
-   time.sleep(1)
+    now = time.mktime(time.localtime())
+    for gp in mediaGroup:
+        if now - gp['timestamp'] >= 2:
+            BD.bot.sendMediaGroup(BD.targetChatId, media=gp['media'])
+            mediaGroup.remove(gp)
+    time.sleep(1)
